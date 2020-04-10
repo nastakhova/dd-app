9de7b9b1807da3802c01992cb6678a0f5e2e8998
letrylesha <lunarboxnote@gmail.com>
2020-04-10 12:10:58 +0300

    dd component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css";
 import Dropdown from "./components/dropdown/dropdown.components";
 
-import store from "./store/index";
 import { observer } from "mobx-react";
 
 function App() {
